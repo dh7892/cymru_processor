d66c8a64490fc205df404d055543d13e2b90292b
Dave <dave@dhills.net>
2023-08-08 15:28:10 +0100

    Working with postrgres and basic data types

---
+++ migrations/20230801_cymru.sql
@@ -0,0 +1,13 @@
+    CREATE TABLE dave_team_cymru_repfeed (
+    stamp timestamp without time zone,
+    addr inet,
+    notes text,
+    cc text,
+    reputation_key text,
+    proto integer,
+    family text,
+    asn integer,
+    category text,
+    reputation_score integer,
+    port integer
+);
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,47 @@
+use chrono::{DateTime, NaiveDateTime, ParseError};
 use flate2::read::GzDecoder;
 use quick_xml::de::from_str;
 use quick_xml::events::Event;
 use quick_xml::reader::Reader as XmlReader;
 use serde::Deserialize;
+use sqlx::types::ipnetwork::IpNetwork;
+use sqlx::{migrate::MigrateDatabase, PgPool, Pool, Sqlite, SqlitePool};
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
-use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool, Pool};
 
-const DB_URL: &str = "sqlite://sqlite.db";
-const DB_URL2: &str = "postgres://postgres:postgres@localhost:5432/postgres";
+const DB_URL: &str = "postgres://postgres:postgres@localhost:5432/postgres";
 
 extern crate prog_rs;
 
 use prog_rs::prelude::*;
 
-async fn process_reputation(reputation: Reputation, db: &SqlitePool) {
+async fn process_reputation(reputation: Reputation, db: &PgPool) {
     // Do something with record
     // println!("{:?}", reputation);
     // Insert the data into our database
-    let result = sqlx::query("INSERT INTO reputation (stamp, addr, notes, cc, reputation_key, proto, family, asn, category, reputation_score, port) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
-        .bind(reputation.stamp)
-        .bind(reputation.addr)
-        .bind(reputation.notes)
-        .bind(reputation.cc)
-        .bind(reputation.reputation_key)
-        .bind(reputation.proto)
-        .bind(reputation.family)
-        .bind(reputation.asn)
-        .bind(reputation.category)
-        .bind(reputation.reputation_score)
-        .bind(reputation.port)
-        .execute( db).await.unwrap();
+    match sqlx::query!(r#"INSERT INTO dave_team_cymru_repfeed (stamp, addr, notes, cc, reputation_key, proto, family, asn, category, reputation_score, port) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)"#
+        ,reputation.stamp
+        ,reputation.addr
+        ,reputation.notes
+        ,reputation.cc
+        ,reputation.reputation_key
+        ,reputation.proto
+        ,reputation.family
+        ,reputation.asn
+        ,reputation.category
+        ,reputation.reputation_score
+        ,reputation.port)
+        .execute(db).await {
+            Ok(_) => println!("Inserted record"),
+            Err(e) => println!("Error inserting record: {}", e ),
+        }
 }
 
-async fn parse_xml(reader: BufReader<GzDecoder<prog_rs::FileProgress>>, db: Pool<Sqlite>) -> quick_xml::Result<()> {
+async fn parse_xml(
+    reader: BufReader<GzDecoder<prog_rs::FileProgress>>,
+    db: PgPool,
+) -> quick_xml::Result<()> {
     let mut reader = XmlReader::from_reader(reader);
     reader.trim_text(true);
 
@@ -101,7 +108,7 @@ async fn parse_xml(reader: BufReader<GzDecoder<prog_rs::FileProgress>>, db: Pool
     Ok(())
 }
 
-async fn read_gz_file<P: AsRef<Path>>(path: P, db: Pool<Sqlite> ) -> std::io::Result<()> {
+async fn read_gz_file<P: AsRef<Path>>(path: P, db: PgPool) -> std::io::Result<()> {
     let file = File::open(path)
         .unwrap()
         .progress()
@@ -119,70 +126,35 @@ async fn read_gz_file<P: AsRef<Path>>(path: P, db: Pool<Sqlite> ) -> std::io::Re
     Ok(())
 }
 
-
 #[tokio::main]
 async fn main() {
-    if !Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
-        println!("Creating database {}", DB_URL);
-        match Sqlite::create_database(DB_URL).await {
-            Ok(_) => println!("Create db success"),
-            Err(error) => panic!("error: {}", error),
-        }
-    } else {
-        println!("Database already exists");
-    }
-    
-    let db = SqlitePool::connect(DB_URL).await.unwrap();
-    let result = sqlx::query("CREATE TABLE IF NOT EXISTS reputation (id INTEGER PRIMARY KEY NOT NULL, 
-    stamp VARCHAR(250) NOT NULL,
-    addr VARCHAR(250) NOT NULL,
-    notes VARCHAR(250) NOT NULL,
-    cc VARCHAR(250) NOT NULL,
-    reputation_key VARCHAR(250) NOT NULL,
-    proto VARCHAR(250),
-    family VARCHAR(250),
-    asn VARCHAR(250) NOT NULL,
-    category VARCHAR(250) NOT NULL,
-    reputation_score VARCHAR(250) NOT NULL,
-    port VARCHAR(250));").execute(&db).await.unwrap();
-    println!("Create user table result: {:?}", result);
-//     // read_gz_file("cut.xml.gz").unwrap();
+    let db = PgPool::connect(DB_URL).await.unwrap();
     read_gz_file("cut.xml.gz", db).await.unwrap();
-
 }
-#[derive(Deserialize, Debug)]
+#[derive(Clone, Deserialize, Debug)]
 struct Reputation {
-    stamp: String,
-    addr: String,
+    #[serde(deserialize_with = "from_timestamp")]
+    stamp: NaiveDateTime,
+    addr: IpNetwork,
     notes: Option<String>,
     cc: String,
     reputation_key: String,
-    proto: Option<String>,
+    proto: Option<i32>,
     family: Option<String>,
-    asn: String,
+    asn: i32,
     category: String,
-    reputation_score: String,
-    port: Option<String>,
+    reputation_score: i32,
+    port: Option<i32>,
 }
 
-#[test]
-fn test_deserialize_xml() {
-    let data = "<repfeed version=\"2\" generated=\"2023-07-31 14:00:00\">
-        <reputations>
-            <reputation>
-                <stamp>2023-07-30 14:00:00</stamp>
-                <addr>173.231.184.122</addr>
-                <notes>hostname: pywolwnvd.biz;</notes>
-                <cc>US</cc>
-                <reputation_key>A29B0C10000D8E0F0G0H0I10000J0K2</reputation_key>
-                <family>http_post</family>
-                <asn>32475</asn>
-                <category>controller</category>
-                <reputation_score>25</reputation_score>
-                <port>80</port>
-            </reputation>
-        </reputations>
-    </repfeed>";
+fn from_timestamp<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let naive = NaiveDateTime::parse_from_str(&s, "%Y-%m-%d %H:%M:%S").unwrap();
+    let datetime: DateTime<chrono::offset::Utc> = DateTime::from_utc(naive, chrono::offset::Utc);
+    Ok(datetime.naive_utc())
 }
 
 #[test]
